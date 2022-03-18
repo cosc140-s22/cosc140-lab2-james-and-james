53997be4556e1fed734b9f257c873b03a132e36a
James Cavallo <jcavallo@colgate.edu>
2022-03-18 13:49:50 +0000

    Mistake

--- lab02.py
+++ lab02.py
@@ -9,3 +9,37 @@ class FoodCategory(Enum):
     OIL = 5
     OTHER = 6
 
+class FoodItem(object):
+    def __init__(self, name, category, calories):
+        try:
+            self.category = FoodCategory(category)
+            self.calories = int(calories)
+        except:
+            print("Invalid!")
+        self.name = self
+    
+    def getName(self):
+        return self.name
+
+    def getCategory(self):
+        return self.category
+
+    def getCalories(self):
+        return self.calories
+
+    def __str__(self):
+        return f'{self.name} ({self.category}) {self.calories}cal/100g'
+    
+
+
+
+
+class FoodServing(object):
+    pass
+
+class Meal(object):
+    pass
+
+
+
+
